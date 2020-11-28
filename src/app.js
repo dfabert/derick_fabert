3f3311a42bef561b56a81bd145336361b51e6422
Glen D Fabert <glen.fabert@gmail.com>
2020-11-27 22:39:45 -0500

    updated app.js with header and switch

--- src/app.js
+++ src/app.js
@@ -3,9 +3,17 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+        <div>
+            <Header />
+                <Switch>
+                    <Route path = '/' component={Home} exact/>
+                    <Route path = '/portfolio' component={Portfolio}/>
+                    <Route path = '/contact' component={Contact}/>
+                    <Route component={Error}/>
+                </Switch>
+        </div>    
+    </BrowserRouter>
   );
 }
 
