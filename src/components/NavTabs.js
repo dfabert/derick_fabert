d6fe024ec17e0e32d29adb3d57977d097497c983
Glen D Fabert <glen.fabert@gmail.com>
2020-11-29 02:21:35 -0500

    navbar updates and spacing on about me page

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,34 +1,28 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function NavTabs() {
-  const location = useLocation();
+  var activeKey = '1';
+  const handleSelect  = (eventKey) => activeKey = '${eventKey}';
 
   return (
-  <nav class="navbar navbar-expand-sm navbar-light bg-dark">
-    <div class="navbar-brand" style={{color: "lightgray"}}>Glen Derick Fabert</div>
-      <div id="navbarNav">
-        <ul class="navbar-nav" >
-          <li class="nav-item">
-            <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-              About Me
-            </Link>
-          </li>
-          <li class="nav-item">
-            <Link to="/Portfolio" className={location.pathname === "/Portfolio" ? "nav-link active" : "nav-link"}>
-              Portfolio
-            </Link>
-          </li>
-          <li class='dropdown'>
-            <div class="nav-link" style={{color: "blanchedalmond"}}>Contact</div>
-            <div class="dropdown-content">
-              <a href="tel:1-317-258-0021">Phone:  (317) 258-0021</a>
-              <a href="mailto:glen.fabert@gmail.com">E-mail:  glen.fabert@gmail.com</a>
-            </div>
-          </li>
-        </ul>
-      </div> 
-  </nav>
+<>
+  <Navbar bg="dark" variant="dark" sticky='top'>
+    <Navbar.Brand href="/">Glen Derick Fabert</Navbar.Brand>
+    <Nav className="justify-content-center" variant='pills' onSelect={handleSelect}>
+      <Nav.Link eventKey='1' href="/">Home</Nav.Link>
+      <Nav.Link eventKey='2' href="/portfolio">Portfolio</Nav.Link>
+      <NavDropdown title="Contact" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1"><a href="tel:1-317-258-0021">Phone:  (317) 258-0021</a></NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4"><a href="mailto:glen.fabert@gmail.com">E-mail:  glen.fabert@gmail.com</a></NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar>
+</>
   );
 }
 
