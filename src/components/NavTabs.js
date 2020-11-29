68f359c87fa8b3aef81f452f270d310fdd11ea8d
Glen D Fabert <glen.fabert@gmail.com>
2020-11-29 01:30:03 -0500

    formated navbar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -5,16 +5,30 @@ function NavTabs() {
   const location = useLocation();
 
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-          About Me
-        </Link>
-        <Link to="/Portfolio" className={location.pathname === "/Portfolio" ? "nav-link active" : "nav-link"}>
-          Portfolio
-        </Link>
-      </li>
-    </ul>
+  <nav class="navbar navbar-expand-sm navbar-light bg-dark">
+    <div class="navbar-brand" style={{color: "lightgray"}}>Glen Derick Fabert</div>
+      <div id="navbarNav">
+        <ul class="navbar-nav" >
+          <li class="nav-item">
+            <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+              About Me
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link to="/Portfolio" className={location.pathname === "/Portfolio" ? "nav-link active" : "nav-link"}>
+              Portfolio
+            </Link>
+          </li>
+          <li class='dropdown'>
+            <div class="nav-link" style={{color: "blanchedalmond"}}>Contact</div>
+            <div class="dropdown-content">
+              <a href="tel:1-317-258-0021">Phone:  (317) 258-0021</a>
+              <a href="mailto:glen.fabert@gmail.com">E-mail:  glen.fabert@gmail.com</a>
+            </div>
+          </li>
+        </ul>
+      </div> 
+  </nav>
   );
 }
 
--- src/components/pages/ProjectCard.js
+++ src/components/pages/ProjectCard.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 function ProjectCard(props) {
     return (
-      <div class="container-sm">
-          {props.projects.map(item => (
-            <div>{item.name}</div>
-          ))}
+      <div>
+          {props.projects.map(project => (
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+                <Card.Title>{project.name}</Card.Title>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
+                <Button variant="primary">Deployed App</Button>
+                <Button variant="primary">Github Link</Button>
+            </Card.Body>
+            </Card>
+            ))}
       </div>
     )
   }
   
   export default ProjectCard;
+  
