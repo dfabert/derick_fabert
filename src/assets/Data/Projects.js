9e10973f70eb506d0fa2c8f1119812066d9e4dca
Glen D Fabert <glen.fabert@gmail.com>
2020-11-29 22:45:34 -0500

    updated data

--- src/assets/Data/Projects.js
+++ src/assets/Data/Projects.js
@@ -17,6 +17,63 @@ const projects = [
       image:  "https://raw.githubusercontent.com/dfabert/PasswordGenerator/master/PassGenPhoto.jpg",
       link: "https://dfabert.github.io/PasswordGenerator/PasswordGenerator.html",
       repo: 'https://github.com/dfabert/PasswordGenerator'
+    },
+    {
+      id: 3,
+      name:  "NBA Dashboard",
+      description: "During the NBA season, this application will pull the scores and times of today's games as well as track your favorite team",
+      technologies: ['HTML5', 'CSS', 'Javascript', 'API'],
+      image:  "https://dfabert.github.io/DerickFabert/assets/Photos/thumbNBADB.jpg",
+      link: "https://dfabert.github.io/NBADashboard/index.html",
+      repo: 'https://github.com/dfabert/NBADashboard'
+    },
+    {
+      id: 4,
+      name:  "Weather Dashboard",
+      description: "This weather dashboard is designed for people who want to look at different cities multiple times. It initially loads with Indianapolis current conditions and five day forecast. However, if the user has used the same browser before, their last searched city will show up. On the left, the user can type in the box a new city to pull the conditions and forecast for. The city will be saved in the local storage to be called back later.",
+      technologies: ['HTML5', 'CSS', 'Javascript', 'API', 'Local Storage'],
+      image:  "https://github.com/dfabert/WXDashboard/blob/master/assets/deployed.jpg?raw=true",
+      link: "https://dfabert.github.io/WXDashboard/",
+      repo: 'https://github.com/dfabert/WXDashboard'
+    },
+    {
+      id: 5,
+      name:  "Eat The Burger",
+      description: "This is an app that allows the user to keep track of the burgers he or she would like to eat. When they eat the burger, they can hit devour and the burger will move to the other side. Future development will include the option to delete the burgers and give them a rating.",
+      technologies: ['MySql', 'Express-handlebars','Node', 'HTML5', 'CSS', 'Javascript'],
+      image:  "https://github.com/dfabert/BurgersToEat/blob/main/public/assets/img/deployed.jpg?raw=true",
+      link: "https://safe-forest-88887.herokuapp.com/",
+      repo: 'https://github.com/dfabert/BurgersToEat'
+    }
+    ,
+    {
+      id: 6,
+      name:  "Note Taker",
+      description: "Note taker and organizer with Express",
+      technologies: ['Node', 'Express', 'HTML5', 'CSS', 'Javascript'],
+      image:  "https://github.com/dfabert/NoteTaker/blob/master/public/assets/images/Notes.JPG?raw=true",
+      link: "https://sleepy-lake-76565.herokuapp.com//",
+      repo: 'https://github.com/dfabert/NoteTaker'
+    }
+    ,
+    {
+      id: 7,
+      name:  "Travel Budget",
+      description: "This application will track your deposits and expenses, even when you are offline. Once you return to service, the application will update your online database.",
+      technologies: ['IndexedDB', 'Node', 'Express', 'HTML5', 'CSS', 'Javascript'],
+      image:  "https://github.com/dfabert/TravelBudget/blob/main/public/assets/ScreenGrab.JPG?raw=true",
+      link: "https://shrouded-river-56580.herokuapp.com/",
+      repo: 'https://github.com/dfabert/TravelBudget'
+    }
+    ,
+    {
+      id: 8,
+      name:  "Workout Tracker",
+      description: "This tracker is designed to keep track of recent workouts so the user can see progress and recall old workouts.",
+      technologies: ['MongoDB', 'Node', 'HTML5', 'CSS', 'Javascript'],
+      image:  "https://github.com/dfabert/WorkoutTracker/blob/main/public/img/screenshots/index.JPG?raw=true",
+      link: "https://salty-chamber-71317.herokuapp.com/?id=5fa55f8c2109c900177139f0",
+      repo: 'https://github.com/dfabert/WorkoutTracker'
     }
   ]
 
