7065170c3e363ab91687a6ada984e96df4da7b08
Glen D Fabert <glen.fabert@gmail.com>
2020-12-01 21:17:29 -0500

    cleaned up NavTabs

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -5,16 +5,14 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 function NavTabs() {
-  var activeKey = '1';
-  const handleSelect  = (eventKey) => activeKey = '${eventKey}';
 
   return (
 <>
   <Navbar bg="dark" variant="dark" sticky='top'>
     <Navbar.Brand href="/">Glen Derick Fabert</Navbar.Brand>
-    <Nav className="justify-content-center" variant='pills' onSelect={handleSelect}>
-      <Nav.Link eventKey='1' href="/">Home</Nav.Link>
-      <Nav.Link eventKey='2' href="/portfolio">Portfolio</Nav.Link>
+    <Nav className="justify-content-center" variant='pills'>
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/portfolio">Portfolio</Nav.Link>
       <NavDropdown title="Contact" id="basic-nav-dropdown">
         <NavDropdown.Item href="#action/3.1"><a href="tel:1-317-258-0021">Phone:  (317) 258-0021</a></NavDropdown.Item>
         <NavDropdown.Divider />
