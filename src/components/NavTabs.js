91260f20005a07f796d5aecf9adfeea303adfa2e
Glen D Fabert <glen.fabert@gmail.com>
2020-12-01 21:57:23 -0500

    changed navlink to just link

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,6 +2,7 @@ import React from "react";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from "react-router-dom";
 
 
 function NavTabs() {
@@ -11,8 +12,8 @@ function NavTabs() {
   <Navbar bg="dark" variant="dark" sticky='top'>
     <Navbar.Brand href="/">Glen Derick Fabert</Navbar.Brand>
     <Nav className="justify-content-center" variant='pills'>
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+      <Link to="/">Home</Link>
+      <Link to="/portfolio">Portfolio</Link>
       <NavDropdown title="Contact" id="basic-nav-dropdown">
         <NavDropdown.Item href="#action/3.1"><a href="tel:1-317-258-0021">Phone:  (317) 258-0021</a></NavDropdown.Item>
         <NavDropdown.Divider />
