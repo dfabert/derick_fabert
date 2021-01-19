a6eb9f08eb30a80583dfec843685933354534184
Glen D Fabert <glen.fabert@gmail.com>
2021-01-18 20:16:06 -0500

    adjusted card to have name at the top

--- src/components/pages/ProjectCard.js
+++ src/components/pages/ProjectCard.js
@@ -11,9 +11,9 @@ function ProjectCard(props) {
           {props.projects.map(project => (
             <Col className="d-flex justify-content-center">
               <Card style={{ width: '19rem', margin: '5px' }} >
-              <Card.Img variant="top" src={project.image} />
               <Card.Body>
                   <Card.Title>{project.name}</Card.Title>
+                  <Card.Img variant="top" src={project.image} />
                   <Card.Text>
                       {project.description}
                   </Card.Text>
