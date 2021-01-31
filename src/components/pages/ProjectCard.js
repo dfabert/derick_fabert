b6df4002399ff2f57a255ed5bdf9864ce1d5784e
Glen D Fabert <glen.fabert@gmail.com>
2021-01-30 20:16:56 -0500

    images centered, buttons pushed to bottom

--- src/components/pages/ProjectCard.js
+++ src/components/pages/ProjectCard.js
@@ -10,11 +10,11 @@ function ProjectCard(props) {
       <Row>
           {props.projects.map(project => (
             <Col className="d-flex justify-content-center">
-              <Card style={{ width: '19rem', height: '40rem',  margin: '5px' }} >
+              <Card style={{ width: '20rem', height: '30rem',  margin: '5px' }} >
               <Card.Body>
                   <Card.Title style={{background: "cornflowerblue", color: "white", padding: "4px"}}>{project.name}</Card.Title>
-                  <Card.Img variant="top" src={project.image} />
-                  <Card.Text>
+                  <Card.Img style={{height: '8rem', width: 'auto', maxWidth: '17rem', display: 'block', marginLeft: "auto", marginRight: "auto"}} src={project.image} />
+                  <Card.Text style={{height: '13rem'}}>
                       {project.description}
                   </Card.Text>
                   <Button variant="primary" href = {project.link} target = "_blank">Deployed App</Button>
