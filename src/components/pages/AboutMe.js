9e04a881902ed7c5cbdd30a01f36cfa40f068d8e
Glen D Fabert <glen.fabert@gmail.com>
2021-01-31 17:45:57 -0500

    added photo to main page

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -11,6 +11,9 @@ function AboutMe() {
             <h1>Derick Fabert</h1>
             <h5>Full Stack Web Developer</h5>
           <div>
+            <img src="https://github.com/dfabert/DerickFabert/blob/master/assets/Photos/Derick.Fabert.0181-6x8.jpg?raw=true" style={{float: "left", margin: "8px", width: "150px"}}/>
+          </div>
+          <div>
             <p>I am a full stack web developer with broadcast meteorology, management, and real estate experience.  I recently graduated from Butler's Web Development Bootcamp, but my passion for web development actually started several years ago when I was a broadcast meteorologist.  Using a website to communicate with our viewers facinated me.  </p>
             <p>I bugged the web manager at work so much that he eventually gave me administrative privileges and set up a "test bed" for me to play with.  I spend many late nights developing several of what I called "takeovers" for the weather section of the website.  They were to be used when different significant weather events were in the forecast.  On the night before a tornado outbreak, I deployed my first takeover. The next day, as 8 tornadoes tore through our viewing area, we have live coverage on our website and mobile app.</p>
             <p>I did leave the TV business but the web development bug never really left.  Therefore I enrolled in Butler's Full Stack Web Development to learn the full power of the tools that the web provides.  Boy, if I could go back and remake those takeovers now.  I really enjoy building web applications and finding new ways to improve them over and over.</p>
