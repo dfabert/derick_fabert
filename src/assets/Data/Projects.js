19a87bb5c21a3382c42de480eaee4ffa3faa4aac
Glen D Fabert <glen.fabert@gmail.com>
2021-01-30 21:13:17 -0500

    technologies are on the cards now

--- src/assets/Data/Projects.js
+++ src/assets/Data/Projects.js
@@ -3,7 +3,7 @@ const projects = [
       id: 0,
       name:  "OmniCalculator",
       description: "All of the calculators you need in one place!",
-      technologies: ['React.js', 'HTML5', 'CSS', 'Javascript'],
+      technologies: ['React.js ', 'HTML5 ', 'CSS ', 'Javascript '],
       image:  "https://github.com/dfabert/derick_fabert/blob/main/src/assets/Photos/OmniCalc.JPG?raw=true",
       link: "https://omnicalculator2020.herokuapp.com",
       repo: 'https://github.com/dfabert/CalculatorApp'
@@ -13,7 +13,7 @@ const projects = [
       id: 1,
       name:  "Google Books Save",
       description: "This application allows a user to search for books in google books and then save the books to be read later.",
-      technologies: ['React.js', 'HTML5', 'CSS', 'Javascript'],
+      technologies: ['React.js ', 'HTML5 ', 'CSS ', 'Javascript '],
       image:  "https://github.com/dfabert/GoogleBooks/blob/main/client/public/images/Deployed_Saved.JPG?raw=true",
       link: "https://booklist0823.herokuapp.com/",
       repo: 'https://github.com/dfabert/GoogleBooks'
@@ -33,7 +33,7 @@ const projects = [
       id: 2,
       name:  "Password Generator",
       description: "The Password Generator generates a very secure password through prompts",
-      technologies: ['HTML5', 'CSS', 'Javascript'],
+      technologies: ['HTML5 ' , 'CSS ', 'Javascript '],
       image:  "https://raw.githubusercontent.com/dfabert/PasswordGenerator/master/PassGenPhoto.jpg",
       link: "https://dfabert.github.io/PasswordGenerator/PasswordGenerator.html",
       repo: 'https://github.com/dfabert/PasswordGenerator'
@@ -42,7 +42,7 @@ const projects = [
       id: 3,
       name:  "NBA Dashboard",
       description: "During the NBA season, this application will pull the scores and times of today's games as well as track your favorite team",
-      technologies: ['HTML5', 'CSS', 'Javascript', 'API'],
+      technologies: ['HTML5 ', 'CSS ', 'Javascript ', 'API '],
       image:  "https://dfabert.github.io/DerickFabert/assets/Photos/thumbNBADB.jpg",
       link: "https://dfabert.github.io/NBADashboard/index.html",
       repo: 'https://github.com/dfabert/NBADashboard'
@@ -50,8 +50,8 @@ const projects = [
     {
       id: 4,
       name:  "Weather Dashboard",
-      description: "This weather dashboard is designed for people who want to look at different cities multiple times. It initially loads with Indianapolis current conditions and five day forecast. However, if the user has used the same browser before, their last searched city will show up. On the left, the user can type in the box a new city to pull the conditions and forecast for. The city will be saved in the local storage to be called back later.",
-      technologies: ['HTML5', 'CSS', 'Javascript', 'API', 'Local Storage'],
+      description: "This weather dashboard is designed for people who want to look at different cities multiple times. The city will be saved in the local storage to be called back later.",
+      technologies: ['HTML5 ', 'CSS ', 'Javascript ', 'API ', 'Local Storage '],
       image:  "https://github.com/dfabert/WXDashboard/blob/master/assets/deployed.jpg?raw=true",
       link: "https://dfabert.github.io/WXDashboard/",
       repo: 'https://github.com/dfabert/WXDashboard'
@@ -59,8 +59,8 @@ const projects = [
     {
       id: 5,
       name:  "Eat The Burger",
-      description: "This is an app that allows the user to keep track of the burgers he or she would like to eat. When they eat the burger, they can hit devour and the burger will move to the other side. Future development will include the option to delete the burgers and give them a rating.",
-      technologies: ['MySql', 'Express-handlebars','Node', 'HTML5', 'CSS', 'Javascript'],
+      description: "This is an app that allows the user to keep track of the burgers he or she would like to eat. When they eat the burger, they can hit devour and the burger will move to the other side.",
+      technologies: ['MySql ', 'Express-handlebars ','Node ', 'HTML5 ', 'CSS ', 'Javascript '],
       image:  "https://github.com/dfabert/BurgersToEat/blob/main/public/assets/img/deployed.jpg?raw=true",
       link: "https://safe-forest-88887.herokuapp.com/",
       repo: 'https://github.com/dfabert/BurgersToEat'
@@ -70,7 +70,7 @@ const projects = [
       id: 6,
       name:  "Note Taker",
       description: "Note taker and organizer with Express",
-      technologies: ['Node', 'Express', 'HTML5', 'CSS', 'Javascript'],
+      technologies: ['Node ', 'Express ', 'HTML5 ', 'CSS ', 'Javascript '],
       image:  "https://github.com/dfabert/NoteTaker/blob/master/public/assets/images/Notes.JPG?raw=true",
       link: "https://sleepy-lake-76565.herokuapp.com//",
       repo: 'https://github.com/dfabert/NoteTaker'
@@ -80,7 +80,7 @@ const projects = [
       id: 7,
       name:  "Travel Budget",
       description: "This application will track your deposits and expenses, even when you are offline. Once you return to service, the application will update your online database.",
-      technologies: ['IndexedDB', 'Node', 'Express', 'HTML5', 'CSS', 'Javascript'],
+      technologies: ['IndexedDB ', 'Node ', 'Express ', 'HTML5 ', 'CSS ', 'Javascript '],
       image:  "https://github.com/dfabert/TravelBudget/blob/main/public/assets/ScreenGrab.JPG?raw=true",
       link: "https://shrouded-river-56580.herokuapp.com/",
       repo: 'https://github.com/dfabert/TravelBudget'
@@ -90,7 +90,7 @@ const projects = [
       id: 8,
       name:  "Workout Tracker",
       description: "This tracker is designed to keep track of recent workouts so the user can see progress and recall old workouts.",
-      technologies: ['MongoDB', 'Node', 'HTML5', 'CSS', 'Javascript'],
+      technologies: ['MongoDB ', 'Node ', 'HTML5 ', 'CSS ', 'Javascript '],
       image:  "https://github.com/dfabert/WorkoutTracker/blob/main/public/img/screenshots/index.JPG?raw=true",
       link: "https://salty-chamber-71317.herokuapp.com/?id=5fa55f8c2109c900177139f0",
       repo: 'https://github.com/dfabert/WorkoutTracker'
--- src/components/pages/ProjectCard.js
+++ src/components/pages/ProjectCard.js
@@ -10,13 +10,12 @@ function ProjectCard(props) {
       <Row>
           {props.projects.map(project => (
             <Col className="d-flex justify-content-center">
-              <Card style={{ width: '20rem', height: '30rem',  margin: '5px' }} >
+              <Card style={{ width: '20rem', height: '32rem',  margin: '5px' }} >
               <Card.Body>
                   <Card.Title style={{background: "cornflowerblue", color: "white", padding: "4px"}}>{project.name}</Card.Title>
                   <Card.Img style={{height: '8rem', width: 'auto', maxWidth: '17rem', display: 'block', marginLeft: "auto", marginRight: "auto"}} src={project.image} />
-                  <Card.Text style={{height: '13rem'}}>
-                      {project.description}
-                  </Card.Text>
+                  <Card.Text style={{height: '7rem'}}>{project.description}</Card.Text>
+                  <Card.Text style={{height: '7rem'}}>Technologies Used:  {project.technologies} </Card.Text>
                   <Button variant="primary" href = {project.link} target = "_blank">Deployed App</Button>
                   <Button variant="primary" href = {project.repo} target = "_blank">Github Repo</Button>
               </Card.Body>
