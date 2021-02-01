ee2df79d4adf4fc5c9db0a846e492aea49981819
Glen D Fabert <glen.fabert@gmail.com>
2021-01-31 19:25:59 -0500

    cleaned up Technologies Used data

--- src/assets/Data/Projects.js
+++ src/assets/Data/Projects.js
@@ -3,7 +3,7 @@ const projects = [
       id: 0,
       name:  "OmniCalculator",
       description: "All of the calculators you need in one place!",
-      technologies: ['React.js ', 'HTML5 ', 'CSS ', 'Javascript '],
+      technologies: ['React.js', 'HTML5', 'CSS', 'Javascript'],
       image:  "https://github.com/dfabert/derick_fabert/blob/main/src/assets/Photos/OmniCalc.JPG?raw=true",
       link: "https://omnicalculator2020.herokuapp.com",
       repo: 'https://github.com/dfabert/CalculatorApp'
@@ -13,7 +13,7 @@ const projects = [
       id: 1,
       name:  "Google Books Save",
       description: "This application allows a user to search for books in google books and then save the books to be read later.",
-      technologies: ['React.js ', 'HTML5 ', 'CSS ', 'Javascript '],
+      technologies: ['React.js', 'HTML5', 'CSS', 'Javascript'],
       image:  "https://github.com/dfabert/GoogleBooks/blob/main/client/public/images/Deployed_Saved.JPG?raw=true",
       link: "https://booklist0823.herokuapp.com/",
       repo: 'https://github.com/dfabert/GoogleBooks'
@@ -23,7 +23,7 @@ const projects = [
       id: 1,
       name:  "Day Planner",
       description: "This application is a single day planner where the user can enter tasks for each hour.  As the day goes on, the background colors will update for the current time as well as the past hours",
-      technologies: ['HTML5 ', 'CSS ', 'Javascript '],
+      technologies: ['HTML5', 'CSS', 'Javascript'],
       image:  "https://raw.githubusercontent.com/dfabert/DayPlanner/master/assets/DeployedImage.jpg",
       link: "https://dfabert.github.io/DayPlanner/",
       repo: 'https://github.com/dfabert/DayPlanner'
@@ -33,7 +33,7 @@ const projects = [
       id: 2,
       name:  "Password Generator",
       description: "The Password Generator generates a very secure password through prompts",
-      technologies: ['HTML5 ' , 'CSS ', 'Javascript '],
+      technologies: ['HTML5' , 'CSS', 'Javascript'],
       image:  "https://raw.githubusercontent.com/dfabert/PasswordGenerator/master/PassGenPhoto.jpg",
       link: "https://dfabert.github.io/PasswordGenerator/PasswordGenerator.html",
       repo: 'https://github.com/dfabert/PasswordGenerator'
@@ -42,7 +42,7 @@ const projects = [
       id: 3,
       name:  "NBA Dashboard",
       description: "During the NBA season, this application will pull the scores and times of today's games as well as track your favorite team",
-      technologies: ['HTML5 ', 'CSS ', 'Javascript ', 'API '],
+      technologies: ['HTML5', 'CSS', 'Javascript', 'API'],
       image:  "https://dfabert.github.io/DerickFabert/assets/Photos/thumbNBADB.jpg",
       link: "https://dfabert.github.io/NBADashboard/index.html",
       repo: 'https://github.com/dfabert/NBADashboard'
@@ -51,7 +51,7 @@ const projects = [
       id: 4,
       name:  "Weather Dashboard",
       description: "This weather dashboard is designed for people who want to look at different cities multiple times. The city will be saved in the local storage to be called back later.",
-      technologies: ['HTML5 ', 'CSS ', 'Javascript ', 'API ', 'Local Storage '],
+      technologies: ['HTML5', 'CSS', 'Javascript', 'API', 'Local Storage'],
       image:  "https://github.com/dfabert/WXDashboard/blob/master/assets/deployed.jpg?raw=true",
       link: "https://dfabert.github.io/WXDashboard/",
       repo: 'https://github.com/dfabert/WXDashboard'
@@ -60,7 +60,7 @@ const projects = [
       id: 5,
       name:  "Eat The Burger",
       description: "This is an app that allows the user to keep track of the burgers he or she would like to eat. When they eat the burger, they can hit devour and the burger will move to the other side.",
-      technologies: ['MySql ', 'Express-handlebars ','Node ', 'HTML5 ', 'CSS ', 'Javascript '],
+      technologies: ['MySql', 'Express-handlebars','Node', 'HTML5', 'CSS', 'Javascript'],
       image:  "https://github.com/dfabert/BurgersToEat/blob/main/public/assets/img/deployed.jpg?raw=true",
       link: "https://safe-forest-88887.herokuapp.com/",
       repo: 'https://github.com/dfabert/BurgersToEat'
@@ -70,7 +70,7 @@ const projects = [
       id: 6,
       name:  "Note Taker",
       description: "Note taker and organizer with Express",
-      technologies: ['Node ', 'Express ', 'HTML5 ', 'CSS ', 'Javascript '],
+      technologies: ['Node', 'Express', 'HTML5', 'CSS', 'Javascript'],
       image:  "https://github.com/dfabert/NoteTaker/blob/master/public/assets/images/Notes.JPG?raw=true",
       link: "https://sleepy-lake-76565.herokuapp.com//",
       repo: 'https://github.com/dfabert/NoteTaker'
@@ -80,7 +80,7 @@ const projects = [
       id: 7,
       name:  "Travel Budget",
       description: "This application will track your deposits and expenses, even when you are offline. Once you return to service, the application will update your online database.",
-      technologies: ['IndexedDB ', 'Node ', 'Express ', 'HTML5 ', 'CSS ', 'Javascript '],
+      technologies: ['IndexedDB', 'Node', 'Express', 'HTML5', 'CSS', 'Javascript'],
       image:  "https://github.com/dfabert/TravelBudget/blob/main/public/assets/ScreenGrab.JPG?raw=true",
       link: "https://shrouded-river-56580.herokuapp.com/",
       repo: 'https://github.com/dfabert/TravelBudget'
@@ -90,7 +90,7 @@ const projects = [
       id: 8,
       name:  "Workout Tracker",
       description: "This tracker is designed to keep track of recent workouts so the user can see progress and recall old workouts.",
-      technologies: ['MongoDB ', 'Node ', 'HTML5 ', 'CSS ', 'Javascript '],
+      technologies: ['MongoDB', 'Node', 'HTML5', 'CSS', 'Javascript'],
       image:  "https://github.com/dfabert/WorkoutTracker/blob/main/public/img/screenshots/index.JPG?raw=true",
       link: "https://salty-chamber-71317.herokuapp.com/?id=5fa55f8c2109c900177139f0",
       repo: 'https://github.com/dfabert/WorkoutTracker'
--- src/components/pages/ProjectCard.js
+++ src/components/pages/ProjectCard.js
@@ -5,6 +5,27 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 function ProjectCard(props) {
+  let techOptions=[];
+    for(let i = 0; i < props.projects.length; i++){
+      let techString='';
+      for(let j = 0; j < props.projects[i].technologies.length; j++){  
+        if(j < props.projects[i].technologies.length-1){
+          techString = techString + props.projects[i].technologies[j] + ", ";
+        }else{
+          techString = techString + " & " + props.projects[i].technologies[j];
+        }
+
+        //Create an array for a dropdown menu
+        if(!techOptions.includes(props.projects[i].technologies[j])){
+          techOptions.push(props.projects[i].technologies[j])
+        }
+
+      }
+      props.projects[i].techString = techString;
+    }
+
+    console.log(techOptions);
+
     return (
     <div>
       <Row>
@@ -15,7 +36,7 @@ function ProjectCard(props) {
                   <Card.Title style={{background: "cornflowerblue", color: "white", padding: "4px"}}>{project.name}</Card.Title>
                   <Card.Img style={{height: '8rem', width: 'auto', maxWidth: '17rem', display: 'block', marginLeft: "auto", marginRight: "auto"}} src={project.image} />
                   <Card.Text style={{height: '7rem'}}>{project.description}</Card.Text>
-                  <Card.Text style={{height: '7rem'}}>Technologies Used:  {project.technologies} </Card.Text>
+                  <Card.Text style={{height: '7rem'}}>Technologies Used:  {project.techString} </Card.Text>
                   <Button variant="primary" href = {project.link} target = "_blank">Deployed App</Button>
                   <Button variant="primary" href = {project.repo} target = "_blank">Github Repo</Button>
               </Card.Body>
