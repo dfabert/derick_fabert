61f7213680a6bd64ad6c51e6134cfc760e8c9819
Glen D Fabert <glen.fabert@gmail.com>
2021-03-09 20:24:12 -0500

    fixed from string to variable

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 function App() {
 
   return (
-      <Router basename={'process.env.PUBLIC_URL'}>
+      <Router basename={process.env.PUBLIC_URL}>
         <div>
           <NavTabs />
           <Route exact path="/" component={AboutMe} />
