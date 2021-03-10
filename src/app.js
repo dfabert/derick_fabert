dc73003a7dd2d10c5f0c0e0f59ba10634174c51b
Glen D Fabert <glen.fabert@gmail.com>
2021-03-09 19:25:47 -0500

    fixed url issues

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 function App() {
 
   return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <div>
           <NavTabs />
           <Route exact path="/" component={AboutMe} />
