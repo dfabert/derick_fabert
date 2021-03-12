7d5f2b26c936e34bb07b8e1329df37028ae7ce8a
Glen D Fabert <glen.fabert@gmail.com>
2021-03-11 19:56:03 -0500

    contact dropdown is now responsive and right aligned

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -9,15 +9,17 @@ function NavTabs() {
 
   return (
     <>
-      <Navbar expand = 'md' bg="dark" variant="dark" sticky='top' >
+      <Navbar bg="dark" variant="dark" sticky='top' >
         <Navbar.Brand as={Link} to="/" >Derick Fabert</Navbar.Brand>
         <Nav className = 'ml-auto'>
           <Nav.Link as={Link} to="/">Home</Nav.Link>
           <Nav.Link as={Link}  to="/portfolio">Portfolio</Nav.Link>
           <NavDropdown alignRight title="Contact" id="collasible-nav-dropdown">
+            <div style={{right: 0, left: "auto"}}>
             <NavDropdown.Item><a href="tel:1-317-258-0021">Phone:  (317) 258-0021</a></NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item><a href="mailto:glen.fabert@gmail.com">E-mail:  glen.fabert@gmail.com</a></NavDropdown.Item>
+            </div>
           </NavDropdown>
         </Nav>
       </Navbar>
